fd7b0a2453ab9b0b6d9ee35a6529b4d530e9f1d6
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-03-31 09:54:30 +0700

    preparing field of product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    /**
+     * Define flag_variant field
+     *
+     * @var mixed
+     */
+    CONST FLAG_VARIANT_AS_PRODUCT = 'P';
+    CONST FLAG_VARIANT_AS_VARIANT = 'V';
+
+    /**
+     * Mass fillable field
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'product_category_id',
+        'name',
+        'description',
+        'is_new',
+        'weight_gram',
+        'flag_variant'
+    ];
+
+    /**
+     * Attribute should hidden
+     *
+     * @var hidden
+     */
+    protected $hidden = [
+        'product_variant_id',
+        'created_at',
+        'updated_at'
+    ];
 }
