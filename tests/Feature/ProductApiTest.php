703647b830ae3e903955956e2d029cc50e614c86
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-04-01 12:47:47 +0700

    fix test code of product api test

--- tests/Feature/ProductApiTest.php
+++ tests/Feature/ProductApiTest.php
@@ -9,8 +9,6 @@ use Tests\TestCase;
 
 class ProductApi extends TestCase
 {
-    use WithFaker;
-
     /**
      * A basic feature test example.
      *
@@ -18,16 +16,8 @@ class ProductApi extends TestCase
      */
     public function test_can_view_product_detail()
     {
-        $product = $this->product_service->storeData([
-            'product_category_id' => $this->faker->randomDigitNotZero(),
-            'name' => $this->faker->text(5),
-            'description' => $this->faker->text(),
-            'is_new' => $this->faker->boolean(),
-            'weight_gram' => $this->faker->randomFloat(),
-            'flag_variant' => $this->faker->randomElement([ Product::FLAG_VARIANT_AS_PRODUCT, Product::FLAG_VARIANT_AS_VARIANT ])
-        ]);
-
-        $response = $this->get('/api/products/' . $product->id, $this->api_headers);
+        $product = Product::factory()->create();
+        $response = $this->get(route('api.products.show', [ 'product' => $product->id ]), $this->api_headers);
 
         $response->assertStatus(200);
     }
@@ -40,7 +30,7 @@ class ProductApi extends TestCase
      */
     public function test_can_not_view_product_detail_if_not_exists()
     {
-        $response = $this->get(route('api.products.show', [ 'products' => 999 ]), $this->api_headers);
+        $response = $this->get(route('api.products.show', [ 'product' => 999 ]), $this->api_headers);
         $response->assertStatus(404);
     }
 }
