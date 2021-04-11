7b6ffc4e571595cdea66c78cb3ea853d0522ff3f
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-04-11 18:50:39 +0700

    add softdeletes to products table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     /**
      * Define flag_variant field
@@ -40,7 +41,8 @@ class Product extends Model
      */
     protected $hidden = [
         'created_at',
-        'updated_at'
+        'updated_at',
+        'deleted_at'
     ];
 
 
