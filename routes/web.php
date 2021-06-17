0816358a0c8c2fc7cdf479b934f250d2a5af6a3f
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-06-17 09:37:35 +0700

    fix login redirect bug

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/login', function() {
-    return redirect('/');
-})->name('login');
